aee8a8fbf6dd5bcca3a6ea0fe1a212aa2c8510b0
Vasil Velikov <vasil.velikov@orbisagency.com>
2022-11-28 13:59:39 +0200

    Added source code

---
+++ Data/MvcMovieContext.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+    public class MvcMovieContext : IdentityDbContext
+{
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie.Models.Movie> Movie { get; set; } = default!;
+    }
---
+++ Models/SeedData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return; // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "Raiders of the Lost Ark",
+                        ReleaseDate = DateTime.Parse("1981-6-12"),
+                        Genre = "Adventure/Action",
+                        Price = 389.9M,
+                        Rating = "S",
+                        ImageURL = "raiders_of_the_lost_ark.jpeg"
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
+
